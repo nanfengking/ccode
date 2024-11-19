6becbfd793c4d0ed57e762da5607d71d40642168
nanfengking <3776583618@qq.com>
2024-11-19 20:24:09 +0800

    Add files via upload

---
+++ test_11_18/test_11_18/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+//{
+//	char arr[] = { 'b','i','t' };
+//	printf("%d\n", strlen(arr));
+//	return 0;
+//}
+{
+	printf("%d\n",
+		strlen("c:\test\121"));
+	return 0;
+
+}
