d48f37346ddb1c0191a0f794efbb458010f57053
nanfengking <3776583618@qq.com>
2024-11-21 23:19:50 +0800

    Add files via upload

---
+++ learn11_21/lianxi1.c
@@ -0,0 +1,45 @@
+//#include <stdio.h>
+//
+//{
+//	int a = 20;
+//	int b = 0;
+//	b = a + 5;
+//	     return 0;
+//}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	char s[] = "\\141\141abc\t";
+//		printf("%d\n", strlen(s));
+//		return 0;
+//}
+   //这是一个ASC||代码查找器，可以考试时候用
+//#include<stdio.h>
+//int main() {
+//	char a;
+//	 a= 'Z';
+//	printf("%c\n",a);
+//	printf("%d\n",a);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//#include <stdbool.h>
+//int main() {
+//	_Bool flag = ture;
+//	if (flag)
+//	printf("i like C\n");
+//	return 0;
+//}
+
+//这个不知道怎么错的，到时候看看
+#include <stdio.h>
+#include <climits>
+
+int main() {
+	LLONG_MIN; char n=11111111111111111111 ;
+	printf("% d\n" , n);
+	return 0;
+}
